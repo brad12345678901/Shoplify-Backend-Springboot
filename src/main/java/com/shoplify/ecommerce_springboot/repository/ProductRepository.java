141bc586a15b70c67ecf97ca0e66c763f4e7c8f8
Brad <ibanezbradley@gmail.com>
2026-05-18 03:00:46 +0800

    Enable SQL Debug Log

--- src/main/java/com/shoplify/ecommerce_springboot/repository/ProductRepository.java
+++ src/main/java/com/shoplify/ecommerce_springboot/repository/ProductRepository.java
@@ -2,6 +2,12 @@ package com.shoplify.ecommerce_springboot.repository;
 
 import com.shoplify.ecommerce_springboot.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+//    @Query("SELECT p FROM ")
+//    Optional<List<Product>> findProductsWithImages();
 }
